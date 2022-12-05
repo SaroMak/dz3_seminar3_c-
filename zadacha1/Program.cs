ad1b7a1cbd952b104225eb3d53dbfc2776b0dddb
«Saro» <opac4321@mail.ru>
2022-12-05 17:45:41 +0300

    переделали задачу 1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -2,26 +2,49 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+// Console.Clear();
+
+// Console.WriteLine("Введите пятизначное число: ");
+
+// int number = int.Parse(Console.ReadLine());
+
+// void VerifyPolindrome(string str)
+// {
+//     Console.WriteLine(str);
+//     int ostatok = number/1000;
+//     int ostatokEnd = number % 100;
+//     int ostatokEnd2 = (ostatokEnd % 10) * 10 + (ostatokEnd /10 );
+//     if (ostatok == ostatokEnd2)
+//         Console.WriteLine ($"число {number} является полиндромом");
+//     else 
+//         Console.WriteLine ($"число {number}  не является полиндромом");
+// }
+// VerifyPolindrome("Проверяем на полиндромность: ");
 
 Console.Clear();
 
 Console.WriteLine("Введите пятизначное число: ");
 
-int number = int.Parse(Console.ReadLine());
-
 void VerifyPolindrome(string str)
+
 {
-     Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(str);
+
     int ostatok = number/1000;
+
     int ostatokEnd = number % 100;
+
     int ostatokEnd2 = (ostatokEnd % 10) * 10 + (ostatokEnd /10 );
+
     if (ostatok == ostatokEnd2)
+
         Console.WriteLine ($"число {number} является полиндромом");
+
     else 
+
         Console.WriteLine ($"число {number}  не является полиндромом");
 }
-
-
-
 
 VerifyPolindrome("Проверяем на полиндромность: ");
